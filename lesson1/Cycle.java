2ef767297eeeac543de0db9671a95c8d5c62279d
livorish <hoksini@gmail.com>
2019-11-24 07:56:20 +0300

    delete something

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,16 +1,18 @@
-class Cycle{
+class Cycle {
 	
 	public static void main(String[] argv) {
 		// task 1. Выведите на консоль с помощью цикла for все числа от [0, 20]
 		for(int i = 0; i < 21; i++){
 			System.out.println("Number: " + i);
-		}	
+		}
+		
 		// task 2. Выведите на консоль с помощью цикла while все числа от [6, -6] (шаг итерации равен 2)
 		int a = 6;
 		while(a > -7){
 			System.out.println("Digit: " + a);
 			a -= 2;
 		}
+		
 		// task 3. Выведите на консоль с помощью цикла do-while сумму всех нечетных чисел от [10, 20]
 		int b = 10;
 		do {
@@ -19,6 +21,5 @@ class Cycle{
 			}			
 			b++;
 		} while (b < 21);
-	}
-	
+	}	
 }
--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,21 +1,15 @@
-class Variable{
+class Variable {
 	
 	public static void main(String[] argv) {
-		// Whole number / simbol(words)
 		char distro = 'U';	
-		// Integers numbers
 		byte bits = 32;
 		short cache =  2048;
 		int networkSpeed = 1_000_000;
 		long hardDrive = 500_000_000_000L;
-		// real number
 		float clockSpeeds = 2.80f;
-		double hddTotalSize = 0.8202;	
-		// string / The String class is not a primitive
-		String CPU = "Pentium(R) Dual-Core CPU E6300";	
-		// logical
+		double hddTotalSize = 0.8202;		
 		boolean isDesktop = true;
-		// output
+
 		System.out.printf("****************************  \n");
 		System.out.printf("-----Hardware information----  \n \n \n");
 		System.out.printf("The bitness of the system: %d \n" , bits);
@@ -25,7 +19,6 @@ class Variable{
 		System.out.printf("Clock speed of the computer: %a  \n", clockSpeeds);
 		System.out.printf("HDD Total Size in bytes in terabytes: %e  \n", hddTotalSize);
 		System.out.printf("Which of OS system: %c  \n", distro);
-		System.out.printf("The processor name: %s  \n", CPU);
 		System.out.printf("Is it a desctop computer? %b  \n \n \n", isDesktop);
 		System.out.println("****************************");
 	}
