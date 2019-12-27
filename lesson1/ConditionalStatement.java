183b114723305aa2ec1288741a61d40233495f67
livorish <hoksini@gmail.com>
2019-12-27 12:14:08 +0300

    Added changes. Change name of a variable.

---
+++ lesson1/ConditionalStatement.java
@@ -0,0 +1,34 @@
+class ConditionalStatement {
+	
+	public static void main(String[] argv) {				
+		int age = 21;
+		if (age > 20) {
+			System.out.println("You are an adult. Welcome! ");
+		}
+		
+		boolean isMan = true;
+		if (isMan) {
+			System.out.println("You are a man");
+		} 
+		
+		if (!isMan) {
+			System.out.println("You are not a man.");
+		}
+		
+		float growth = 1.20f;
+		if (growth < 1.80) {
+			System.out.println("You are short.");
+		} else {
+			System.out.println("Wow, you are tall!");
+		}
+		
+		char firstLetterOfName = 'O';
+		if (firstLetterOfName == 'M') {
+			System.out.println("Your name starts with the letter M");
+		} else if (firstLetterOfName == 'I') {
+			System.out.println("Your name starts with the letter I");
+		} else {
+			System.out.println("Your name starts with an unknown letter");
+		}	
+	}
+}
