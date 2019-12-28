c759f3d043b476054345ea5eeb7c5ca639d36ed9
livorish <hoksini@gmail.com>
2019-12-29 02:38:51 +0300

    Changed - delete unnecessary strings
    Delete extra unnecessary variables

--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -2,8 +2,7 @@ class Unicode {
 	
 	public static void main(String[] args) {
 		for (int i = 9398; i <= 10178; i++) {
-			char ch = (char) i;
-			System.out.println(ch);
+			System.out.println((char) i);
 		}
 	}
 }
